3742145e63fcea21d517a98c7682dd0a21970945
Mohammad Taha <m.taha10045@gmail.com>
2025-04-27 16:36:52 +0500

    chnages in composer.json and test migrations

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migrations', function () {
+    try {
+        $deployToken = request()->header('X-DEPLOY-TOKEN');
+        if ($deployToken !== env('DEPLOY_TOKEN')) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        Artisan::call('migrate --force');
+        return response()->json(['message' => 'Migrations run successfully']);
+    } catch (\Exception $e) {
+        return response()->json(['message' => 'Failed to run migrations: ' . $e->getMessage()], 500);
+    }
 });
